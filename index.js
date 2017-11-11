3ed6869d7a8a010bdaf60b0193981a7a76a604f1
Dana Evernden <devernde@gmail.com>
2017-11-11 20:01:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function happyHolidayTo(holiday, name) {
   return 'Happy ${holiday}, ${name}!';
 }
 function holidayCountdown(days, holiday){
-  return 
+  return `It's ${days} days until ${holiday}!`
 }
